100e2919605abbca20a69beac41d891ce14e4061
Apingcysec <149550490+burndevl@users.noreply.github.com>
2025-09-22 19:34:14 +0800

    Update mencari_luas_lingkaran.cpp

--- mencari_luas_lingkaran.cpp
+++ mencari_luas_lingkaran.cpp
@@ -2,19 +2,31 @@
 
 using namespace std;
 
+int main() {
+    int pilihan;
     int r;
     const float pi = 3.14;
-    float rumus_luas;
 
+    cout << "==== Menu Lingkaran ====\n";
+    cout << "1. Hitung luas\n";
+    cout << "2. Hitung keliling\n";
+    cout << "Masukkan pilihan: ";
+    cin >> pilihan;
 
-int main() {
+    switch(pilihan) {
+        case 1:
+            cout << "Masukkan jari-jari: ";
+            cin >> r;
+            cout << "Luas lingkaran: " << r * r * pi << " cm2" << endl;
+            break;
+        case 2:
+            cout << "Masukkan jari-jari: ";
+            cin >> r;
+            cout << "Keliling lingkaran: " << 2 * r * pi << " cm" << endl;
+            break;
+        default:
+            cout << "pilihan kamu tidak ada." << endl;
+    }
 
-    cout << "==== MENCARI LUAS LINGKARAN ====\n";
-    cout << "Masukan Nilai Jari - Jari : ";
-    cin >> r;
-
-    rumus_luas = r * r * pi;
-
-    cout << "\nLuas Lingkarannya Adalah : " << rumus_luas << " cm2";
-
+    return 0;
 }
